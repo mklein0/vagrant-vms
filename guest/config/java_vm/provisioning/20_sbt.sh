4b579a956c0556c54e977d4686bc7f6c927a317f
Marcos Klein <mklein0@gmail.com>
2016-10-04 22:20:26 -0700

    Add scala debian packages to install.

---
+++ guest/config/java_vm/provisioning/20_sbt.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env false
+
+echo "deb https://dl.bintray.com/sbt/debian /" | tee -a /etc/apt/sources.list.d/sbt.list
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 642AC823
---
+++ guest/config/java_vm/provisioning/30_sbt.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env false
+
+apt-get install ${APT_GET_OPTS} sbt
